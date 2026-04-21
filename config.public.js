4d02b3cc45d5ae24a5e2c8fa11829ab4bb69a8dd
Soeler <102874847+soeler1337@users.noreply.github.com>
2026-04-21 10:48:03 +0200

    fix: add SITE_ID to config.public.js on Rel branch

--- config.public.js
+++ config.public.js
@@ -1,4 +1,5 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://gchuountldrnjfpwhnpc.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_LpaftROJx30RmCNQ4EldwA_jqHi53bI"
+  SUPABASE_ANON_KEY: "sb_publishable_LpaftROJx30RmCNQ4EldwA_jqHi53bI",
+  SITE_ID: "pixelwg"
 };
